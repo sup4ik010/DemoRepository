fe4a84c334f1898a8ca3ce70a4d87e89e2aa648c
sup4ik010 <97286261+sup4ik010@users.noreply.github.com>
2022-05-04 09:27:04 +0300

    add UnitTests

---
+++ 7/DemoLibrary.Tests/CollectionComparisonTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnitTests;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CollectionComparisonTests
+    {   
+        [Fact]
+        public void AreTwoCollectionsEqual_ComparisonShouldWork()
+        {
+            var expected = true;
+
+            var firstCollection = new List<string>();
+            var secondCollection = new List<string>();
+            var actual = CollectionComparison.AreTwoCollectionsEqual(firstCollection, secondCollection);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AreTwoCollectionsEqual_ComparisonShouldFail()
+        {
+            var expected = false;
+
+            var firstCollection = new List<string>();
+            var secondCollection = new List<string>() { "1", "2"};
+
+            var actual = CollectionComparison.AreTwoCollectionsEqual(firstCollection, secondCollection);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsCollectionNull_ShouldWork()
+        {
+            var expected = false;
+
+            var actual = CollectionComparison.IsColectionNull(new List<string>());
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsCollectionNull_ShouldFail()
+        {
+            Assert.Throws<ArgumentNullException>(() => CollectionComparison.IsColectionNull(null));
+        }
+    }
+}
---
+++ 7/DemoLibrary/CollectionComparison.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTests
+{
+    public static class CollectionComparison
+    {
+        #region Methods
+
+        public static bool AreTwoCollectionsEqual(List<string> firstCollection, List<string> secondCollection)
+        {
+            IsColectionNull(firstCollection);
+            IsColectionNull(secondCollection);
+
+            return firstCollection.SequenceEqual(secondCollection);
+        }
+
+        public static bool IsColectionNull(List<string> collection)
+        {
+            if (collection is null)
+            {
+                throw new ArgumentNullException("Коллекция не может быть null.", nameof(collection));
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
